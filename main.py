9d641b5b2a733d489ff2fbb4ee4e5774182993e6
Becelli <becelli@protonmail.com>
2021-11-24 16:39:54 -0300

    fixing imports

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ cars = sql.connect("./db/application.db")
 cursor = cars.cursor()
 
 # Classes dependencies
-import classes.car.national as nc
-import classes.car.international as ic
+import functions.database as db
+import classes.user as user
+import classes.vehicle as vehicle
+
+db.init()
+# # Start GUI from here
 
 
-# Example of a car
-car1 = nc.national_car(
-    1, "Qwid", "Renault", 2018, 2018, "CHW9797", "Sedan", 34000, 200, True, 0.12
-)
-car2 = ic.international_car(
-    2, "Onix", "Chevrolet", 2021, 2020, "BKV3413", "Sedan", 52300, 200, True, 0.10, 0.05
-)
+test = False
+if test:
+    users = db.select_all_users()
+    n = 3
+    db.populate_user(n)
+    db.populate_vehicle(n)
 
-print(car1.__str__())
-print(car1.calculate_daily_rent_value())
+    users = db.select_all_users()
+    vehicles = db.select_all_cars()
 
-print(car2.__str__())
-print(car2.calculate_daily_rent_value())
+    for user in users:
+        print(user)
+        print("\n--------\n")
+    for vehicle in vehicles:
+        print(vehicle)
